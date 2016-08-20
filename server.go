0424d649989e8a501e3e813202cf86f89eafa5fa
Matt Moore <mattmoore@carbonhelix.com>
2016-08-20 10:21:26 -0500

    Add command support to server

--- server.go
+++ server.go
@@ -8,16 +8,6 @@ import (
 	"time"
 )
 
-func handleConnection(conn net.Conn) {
-	scanner := bufio.NewScanner(bufio.NewReader(conn))
-	for scanner.Scan() {
-		messageTime := time.Now()
-		message := scanner.Text()
-		fmt.Println(messageTime, ": ", message)
-		conn.Write([]byte(fmt.Sprintf("received\n")))
-	}
-}
-
 func main() {
 	listen, _ := net.Listen("tcp", fmt.Sprintf(":%s", os.Args[1]))
 	fmt.Println(fmt.Sprintf("Pyramid server listening on port %s", os.Args[1]))
@@ -25,4 +15,29 @@ func main() {
 		conn, _ := listen.Accept()
 		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	scanner := bufio.NewScanner(bufio.NewReader(conn))
+	for scanner.Scan() {
+		messageTime := time.Now()
+		message := scanner.Text()
+		fmt.Println(messageTime, ": ", message)
+		executeCommand(message)
+		conn.Write([]byte(fmt.Sprintf("received\n")))
+	}
+}
+
+func executeCommand(cmd string) {
+	commands := map[string]func(){
+		"\\q": execQuit,
+	}
+	if commands[cmd] != nil {
+		fmt.Println(fmt.Sprintf("Received command: %s", cmd))
+		commands[cmd]()
+	}
+}
+
+func execQuit() {
+	fmt.Println("Executing quit")
 }
