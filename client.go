593f91b2db948b54ab495b5b3fff4f65e5e26c6d
Matt Moore <mattmoore@carbonhelix.com>
2016-08-19 19:26:05 -0500

    Allow dynamic host and port.

--- client.go
+++ client.go
@@ -1,35 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "net"
-  "bufio"
-  "os"
-  "strings"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
 )
 
+func consoleReady() {
+	fmt.Print("> ")
+}
+
 func main() {
-  conn, err := net.Dial("tcp", "localhost:8080")
-  if err != nil {
-    fmt.Println(err)
-  }
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]))
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  command_reader := bufio.NewReader(os.Stdin)
-  response_reader := bufio.NewReader(conn)
+	commandScanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	responseScanner := bufio.NewScanner(bufio.NewReader(conn))
 
-  for {
-    fmt.Print("> ")
-    text, _ := command_reader.ReadString('\n')
-    text = strings.Replace(text, "\n", "", -1)
-    fmt.Fprintf(conn, text + "\n")
-    response, err := response_reader.ReadString('\n')
-    if err != nil {
-      conn.Close()
-      fmt.Println("Lost connection")
-      break
-    } else {
-      if response == "received\n" {
-      }
-    }
-  }
+	consoleReady()
+
+	for commandScanner.Scan() {
+		text := commandScanner.Text()
+		fmt.Fprintf(conn, text+"\n")
+
+		responseScanner.Scan()
+		if err := responseScanner.Err(); err != nil {
+			conn.Close()
+			fmt.Println("Lost connection")
+			break
+		} else {
+			consoleReady()
+		}
+	}
 }
